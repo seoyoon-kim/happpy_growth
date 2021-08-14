d1119df797c0db3dc157f55efb75f7dacb579e71
seoyoon-kim <76926158+seoyoon-kim@users.noreply.github.com>
2021-08-14 22:56:17 +0900

    08/14

---
+++ src/App.js
@@ -0,0 +1,49 @@
+ 
+import React, { Component } from 'react';
+import TOC from "./components/TOC";
+import Content from "./components/Content";
+import Subject from "./components/Subject";
+import './App.css';
+
+
+ class App extends Component {
+   constructor(props){
+    super(props);
+    this.state = {
+      mode:'welcome',
+      subject:{title:'WEB', sub:'use state!'},
+      welcome:{title:'welcome', desc:'hello react'},
+      contents:[
+        {id:1, title:'HTML', desc:'HTML!!'},
+        {id:2, title:'CSS', desc:'CSS!!'} ,
+        {id:3, title:'JS', desc:'JS!!'} 
+      ]
+    }
+   }
+  render(){
+    var _title, _desc = null; 
+    if(this.state.mode==='welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else if(this.state.mode==='read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+    }
+    return (
+      <div className="App">
+            <header className="App-header">
+              Hello, React! 
+              <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+              <Subject title="React" sub="For UI"></Subject>
+              <TOC data={this.state.contents}></TOC>
+              <Content title={_title} desc={_desc}></Content>
+            </header>
+          </div>
+    );
+  }
+}
+ 
+   
+ 
+
+export default App;
