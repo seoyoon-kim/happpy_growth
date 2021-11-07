ffb299d7f8ac8f92bd698eaea0d150bd1e0da63a
growth <godkmn1004@naver.com>
2021-11-07 22:08:52 +0900

    11/07 nuxt-board2

---
+++ nuxt-board2/ch2/front/store/posts.js
@@ -0,0 +1,34 @@
+export const state = () => ({
+    mainPosts: [],
+  });
+  
+  export const mutations = {
+    addMainPost(state, payload) {
+      state.mainPosts.unshift(payload);//맨 앞에 추가
+    },
+    removeMainPost(state, payload) {
+      const index = state.mainPosts.findIndex(v => v.id === payload.id);
+      state.mainPosts.splice(index, 1);
+    },
+    addComment(state, payload) {
+      const index = state.mainPosts.findIndex(v => v.id === payload.postId);
+      state.mainPosts[index].Comments.unshift(payload);
+    }
+  };
+  
+  export const actions = {
+    add({ commit }, payload) {
+      // 서버에 게시글 등록 요청 보냄
+      commit('addMainPost', payload);
+      /*
+        index.js에도 같은 모듈이 있어 그걸 사용할 때
+        commit('addMainPost', payload, {root:true});
+      */
+    },
+    remove({ commit }, payload) {
+      commit('removeMainPost', payload);
+    },
+    addComment({ commit }, payload) {
+      commit('addComment', payload);
+    },
+  };
