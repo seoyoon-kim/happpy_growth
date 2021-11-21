cf924c2e719a4f9678669016e74a9edbe372af59
growth <godkmn1004@naver.com>
2021-11-21 21:30:07 +0900

    2021/11/21

--- nuxt-board2/ch2/front/store/index.js
+++ nuxt-board2/ch2/front/store/index.js
@@ -1,14 +1,12 @@
-import createPersistedState from "vuex-persistedstate";
-  
 export const state = () => ({
-    hello: 'vuex',
-  });
-  
-  export const mutations = {
-    bye(state) {
-      state.hello = 'goodbye';
-    },
-    addMainPost() {
-  
-    },
-  };
+  hello: 'vuex',
+});
+
+export const mutations = {
+  bye(state) {
+    state.hello = 'goodbye';
+  },
+  addMainPost() {
+
+  },
+};
